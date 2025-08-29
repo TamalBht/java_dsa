bb3ba2fa2cc619326202e9811b04780431dda441
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2025-08-30 02:21:37 +0530

    template definition

--- template.java
+++ template.java
@@ -1,5 +1,15 @@
 public class template{
+    int count =0;
+    public void recur(int n){
+        if(count==n) return;
+        else{
+            count++;
+            System.out.println("Hello world");
+            recur(n);
+        }
+    }
     public static void main(String[]args){
-        System.out.println("Hello World");
+        template t= new template();
+        t.recur(5);
     }
 }
