f5c6e44fa808af6fb70d1fd8acc5838f84d8b8bd
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2025-09-29 00:33:38 +0530

    changes

--- string/MaxDepth.java
+++ string/MaxDepth.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class MaxDepth {
     public int maxDepth(String s) {
         int count=0,maxCount=0;
+        
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='(')count++;
             else if(s.charAt(i)==')'){
