3617bb7a6f82a62461145a1dcd2c345869761188
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2026-01-08 01:06:06 +0530

    Mege SubIntervals bruteforces

--- arrays/MergeSubIntervals.java
+++ arrays/MergeSubIntervals.java
@@ -6,44 +6,30 @@ import java.lang.*;
 //exaample[[1,3],[2,6],[8,10]] output-> [[1,6],[8,10]] logic: sice 2 lies between 1 and 3 => we can mergew them
 public class MergeSubIntervals {
     public int[][] mergee(int[][] intervals){
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);//sort based on the first number
+        int i=0;
+        List<int[]>res=new ArrayList<>();
         int n=intervals.length;
-         List<int[]>res=new ArrayList<>();
-
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<n;i++){
-            Arrays.sort(intervals[i]);
-            map.put(intervals[i][0], i);
-        }
-        
-         int indexIgnored=-1;
-        for(int i=0;i<n;i++){
-            if(i==indexIgnored)continue;
-            if(i>0 && intervals[i][0]==intervals[i-1][0] && intervals[i][1]==intervals[i-1][1])continue;
+        while(i<n){
             int start=intervals[i][0];
             int end=intervals[i][1];
-           int[] temp=new int[2];
-            for(int j=start+1;j<=end;j++){
-
-                if(map.containsKey(j)){
-                    
-                    temp[0]=start;
-                    temp[1]=intervals[map.get(j)][1];
-                    indexIgnored=map.get(j);
-                    res.add(temp);
-                }
+            int j=i+1;
+            while(j<n && intervals[j][0]<=end){
+                end=Math.max(end,intervals[j][1]);
+                j++;
             }
-            if(i!=indexIgnored && temp[0]==temp[1] && temp[0]==0)res.add(intervals[i]);
-
-
+            i=j;
+            int temp[] = {start,end};
+            res.add(temp);
         }
-        int k=res.size();
-        int[][] finall=new int[k][2];
-        for(int i=0;i<k;i++){
-            finall[i]=res.get(i);
+        int[][] finall = new int[res.size()][2];
+         i=0;
+        for(int[]it:res){
+            finall[i]=it;
+            i++;
         }
         return finall;
+
     }
     public static void main(String[] args) {
         int size=Integer.parseInt(args[0]);
