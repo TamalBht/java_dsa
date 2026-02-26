79edd2bb5f1cf42f35fa204879ef90493067dab4
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2026-02-27 02:20:30 +0530

    char to ascii (for alphabets) then the maximu digit in the 2 digit or 3digit number finally return the sum of max of each iteration

---
+++ pracs/ASCIISum.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+//char to ascii (for alphabets) then the maximu digit in the 2 digit or 3digit number finally return the sum of max of each iteration
+public class ASCIISum {
+    private int summ(String s){
+        int sum=0;
+        for(char ch:s.toCharArray()){
+            int maxx= Integer.MIN_VALUE;
+            int asc=0;
+            if(Character.isDigit(ch))asc=ch-'0';
+            else asc=ch;
+            while(asc!=0){
+                maxx=Math.max(maxx,asc%10);
+                asc=asc/10;
+                
+            }
+            sum+=maxx;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        String s="12ab";
+        ASCIISum obj=new ASCIISum();
+        System.out.println(obj.summ(s));
+    }
+}
