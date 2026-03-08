5695e4d8acc05d1d846d30e2a4eda6bdae1caf02
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2026-03-09 02:28:37 +0530

    shorter answer

--- pracs/BitOperation.java
+++ pracs/BitOperation.java
@@ -6,29 +6,41 @@ public class BitOperation {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int inp=sc.nextInt();
-        int count=0;
-        int initBit=0;
-        while(inp/2>0){
-            int bit=inp%2;
-            initBit=initBit+bit*(int)(Math.pow(10,count));
-            count++;
-            inp=inp/2;
-            if(inp==1){
-                initBit=initBit+(int)(Math.pow(10,count));
-                break;
-            }
-        }
-        System.out.println(initBit);
-        int finalRes=0;
-        String s=Integer.toString(initBit);
+        // int count=0;
+        // int initBit=0;
+        // while(inp/2>0){
+        //     int bit=inp%2;
+        //     initBit=initBit+bit*(int)(Math.pow(10,count));
+        //     count++;
+        //     inp=inp/2;
+        //     if(inp==1){
+        //         initBit=initBit+(int)(Math.pow(10,count));
+        //         break;
+        //     }
+        // }
+        // System.out.println(initBit);
+        // int finalRes=0;
+        // String s=Integer.toString(initBit);
         
-        for(int i=0;i<s.length();i++){
-            int temp=s.charAt(i)-'0';
-            if(temp==1)temp=0;
-            else temp=1;
-            temp=(int)(temp*Math.pow(2,s.length()-i-1));
-            finalRes=temp+finalRes;
+        // for(int i=0;i<s.length();i++){
+        //     int temp=s.charAt(i)-'0';
+        //     if(temp==1)temp=0;
+        //     else temp=1;
+        //     temp=(int)(temp*Math.pow(2,s.length()-i-1));
+        //     finalRes=temp+finalRes;
+        // }
+        // System.out.println(finalRes);   
+        int res=0;
+        int i=0;
+        while(inp>0){
+            int bit=inp%2;
+            if(bit==0)bit=1;
+            else bit=0;
+            res=res+bit*(int)(Math.pow(2,i));
+            i++;
+            inp=inp/2;
+            
         }
-        System.out.println(finalRes);        
+        System.out.println(res);     
     }
 }
